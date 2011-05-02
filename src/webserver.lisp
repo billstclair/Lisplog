03a5a6b4baba55ad7a225acf69d343c80a47ffea
Bill St. Clair <billstclair@gmail.com>
2011-05-02 08:24:33 -0400

    Build the basic handler structure

--- src/webserver.lisp
+++ src/webserver.lisp
@@ -35,6 +35,35 @@
     (setf *webserver-acceptor* nil
           *webserver-db* nil)))
 
+;;;
+;;; URL handlers
+;;; You will usually get here via a RewriteRule in your blogs .htaccess file
+;;; I use the "admin" dir to trigger that rule.
+;;; This code would work with another trigger, however.
+;;;
+
+;; <baseurl>/admin/?command=value
+(hunchentoot:define-easy-handler (handle-admin :uri "/")
+    (node edit_post add_comment edit_comment uri https)
+  (setf (hunchentoot:content-type*) "text/html")
+  (acond (node (render-web-node node uri https))
+         (edit_post (edit-post edit_post uri https))
+         (add_comment (add-comment add_comment uri https))
+         (edit_comment (edit-comment edit_comment uri https))
+         (t (not-found))))
+
+;; <baseurl>/admin/settings
+(hunchentoot:define-easy-handler (settings :uri "/settings") (uri https)
+  (format nil "settings, uri: ~s, https: ~s" uri https))
+
+;; <baseurl>/admin/add_post
+(hunchentoot:define-easy-handler (add-post :uri "/add_post") (uri https)
+  (format nil "add post, uri: ~s, https: ~s" uri https))
+
+;;;
+;;; Implementation of URL handlers
+;;;
+
 (defun compute-base-and-home (uri https &optional alias)
   (declare (ignore alias))              ;do later
   (let ((base (format nil "http~a://~a"
@@ -43,9 +72,12 @@
         )
     (values base ".")))
 
-(defun render-node-string (node uri https &key alias (data-db *data-db*))
+;; <baseurl>/admin/?node=<node-num>
+(defun render-web-node (node-num uri https &key alias (data-db *webserver-db*))
+  (unless (setf node-num (ignore-errors (parse-integer node-num)))
+    (return-from render-web-node (not-found)))
   (with-settings ()
-    (let* ((plist (make-node-plist node :data-db data-db))
+    (let* ((plist (make-node-plist node-num :data-db data-db))
            (post-template-name (get-post-template-name data-db)))
       (when plist
         (unless alias
@@ -53,19 +85,28 @@
         (setf plist `(:posts
                       (,plist)
                       :page-title ,(getf plist :title)
-                      ,@(compute-history-plist node data-db)))
+                      ,@(compute-history-plist node-num data-db)))
         (multiple-value-bind (base home) (compute-base-and-home uri https)
           (setf (getf plist :home) home
                 (getf plist :base) base))
         (setf (getf plist :permalink) alias)
         (render-template post-template-name plist :data-db data-db)))))
 
-(hunchentoot:define-easy-handler (handle-admin :uri "/") (node uri https)
-  (setf (hunchentoot:content-type*) "text/html")
-  (acond ((and node (ignore-errors (parse-integer node)))
-          (render-node-string it uri https
-                              :data-db *webserver-db*))
-         (t (setf (hunchentoot:return-code*) hunchentoot:+http-not-found+))))
+(defun not-found ()
+  (setf (hunchentoot:return-code*) hunchentoot:+http-not-found+)
+  "404")
+
+;; <baseurl>/admin/?edit_post=<node-num>
+(defun edit-post (node-num uri https)
+  (format nil "edit post, node: ~s, uri: ~s, https: ~s" node-num uri https))
+
+;; <baseurl>/admin/?add_comment=<node-num>
+(defun add-comment (node-num uri https)
+  (format nil "add comment, node: ~s, uri: ~s, https: ~s" node-num uri https))
+
+;; <baseurl>/admin/?edit_comment=<comment-num>
+(defun edit-comment (comment-num uri https)
+  (format nil "edit comment, comment: ~s, uri: ~s, https: ~s" comment-num uri https))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
